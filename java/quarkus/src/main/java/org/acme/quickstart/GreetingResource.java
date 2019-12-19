11445b89def314bb667382dd06bedf147c890664
Ryan Zhang <rzhang@redhat.com>
2019-12-19 13:52:07 +0800

    Add back greeting text

--- java/quarkus/src/main/java/org/acme/quickstart/GreetingResource.java
+++ java/quarkus/src/main/java/org/acme/quickstart/GreetingResource.java
@@ -1,20 +1,23 @@
 package org.acme.quickstart;
 
-
 import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
+import org.eclipse.microprofile.config.inject.ConfigProperty;
 import org.jboss.resteasy.annotations.jaxrs.PathParam;
 
-@Path("/hello")
+@Path("/")
 public class GreetingResource {
 
     @Inject
     GreetingService service;
 
+    @ConfigProperty(name = "MESSAGE_PREFIX", defaultValue = "Hi")
+    String messagePrefix;
+    
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     @Path("/greeting/{name}")
@@ -23,8 +26,9 @@ public class GreetingResource {
     }
 
     @GET
+    @Path("/")
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
-        return "hello";
+        return messagePrefix;
     }
 }
